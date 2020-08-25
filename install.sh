5db26987414b4e108cbb467f05120dfd9156963f
pdaynoe <52581741+pdaynoe@users.noreply.github.com>
2020-08-25 12:16:57 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
-rm -rf /boot/grub/themes/arch-silence
-cp -TR ./theme /boot/grub/themes/arch-silence
+rm -rf /boot/grub/themes/artix-silence
+cp -TR ./theme /boot/grub/themes/artix-silence
+grep -v "GRUB_THEME" /etc/default/grub  > /etc/default/grub
+echo 'GRUB_THEME="/boot/grub/themes/artix-silence/theme.txt"' >> /etc/default/grub 
 grub-mkconfig -o /boot/grub/grub.cfg
